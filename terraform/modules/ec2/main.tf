530798a9097129bf19c8980c73c57ebba7f0f0c9
Jacob Sanchez <jacob.sanchez@slalom.com>
2023-06-21 16:05:37 -0500

    Added public key for ec2 instance

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "cues-ec2" {
   # Update with your desired AMI ID
   ami = "ami-0b54418bdd76353ce" # Linux AMI ID
   instance_type = "t2.micro"
-  key_name = aws_key_pair.ec2-key-pair.key_name
+  key_name = var.key_name
   security_groups = ["cues-ec2-sg-aft"]
 }
