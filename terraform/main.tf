91304f42dea79a23c00c3933f092f60df3f2351a
Jacob Sanchez <jacob.sanchez@slalom.com>
2023-06-21 16:23:36 -0500

    Ec2 is one module now for ease of use

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,10 +6,6 @@ module "subnet" {
   source = "./modules/subnet"
   vpc_id = module.vpc.vpc_id
 }
-module "security-groups" {
-  source = "./modules/security-groups"
-}
 module "ec2" {
   source = "./modules/ec2"
-  key_name = module.security-groups.key_name
 }
--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -2,11 +2,31 @@ resource "aws_instance" "cues-ec2" {
   # Update with your desired AMI ID
   ami = "ami-0b54418bdd76353ce" # Linux AMI ID
   instance_type = "t2.micro"
-  key_name = var.key_name
+  key_name = aws_key_pair.ec2-key-pair.key_name
   security_groups = ["cues-ec2-sg-aft"]
 }
 
-variable "key_name" {
-  description = "The key name to be used by the EC2 instance"
-  type        = string
+
+resource "aws_security_group" "cues_sg" {
+  name = "cues-sg-using-terraform"
+
+  #Incoming traffic
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["98.212.123.227/32"] #replace it with your ip address
+  }
+
+  #Outgoing traffic
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_key_pair" "ec2-key-pair" {
+  key_name = "ec2_key"
+  public_key = file("./ec2_key_pair.pub")
 }
