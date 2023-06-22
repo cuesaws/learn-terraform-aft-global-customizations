fe221da8e2cf2a7401e4896a9348b4a1f8bec4b6
Jacob Sanchez <jacob.sanchez@slalom.com>
2023-06-22 08:23:14 -0500

    Refactoring ec2 module

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -13,7 +13,7 @@ resource "aws_instance" "cues-ec2" {
   ami = "ami-0b54418bdd76353ce" # Linux AMI ID
   instance_type = "t3.micro"
   key_name = aws_key_pair.ec2-key-pair.key_name
-  security_groups = ["cues-ec2-sg-aft"]
+  security_groups = [aws_security_group.cues_sg.name]
   subnet_id = var.subnet_id
 }
 
